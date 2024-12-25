aed5a2bb719743767adeb4896ba07a34c13964d3
Ahmad Ragab <averroes2006@gmail.com>
2024-12-25 13:27:31 -0500

    unsetting require branch

--- s2/build.gradle.kts
+++ s2/build.gradle.kts
@@ -94,11 +94,15 @@ publishing {
 }
 
 release {
+    // Disable the default checks since we're using GitHub Actions
+    failOnUnversionedFiles = false
+    failOnUpdateNeeded = false
+    failOnCommitNeeded = false
     git {
-        requireBranch.set("main")
-        pushToRemote.set("origin")
-        signTag.set(false)
-        tagTemplate.set("v\${version}")
+      requireBranch.set("") // Since we use GHA to release this should be okay
+      pushToRemote.set("origin")
+      signTag.set(false)
+      tagTemplate.set("v\${version}")
     }
 }
 
