351827e581c540a4126f9b49710ce0585ec3f011
Stephen Balogh <stephen@s2.dev>
2025-01-28 15:32:37 -0800

    [Gradle Release Plugin] - pre tag commit:  'v0.0.5'.

--- s2/build.gradle.kts
+++ s2/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     `java-library`
     id("maven-publish")
-    id("net.researchgate.release") version "3.1.0"
 }
 
 group = "dev.s2"
@@ -89,21 +88,4 @@ publishing {
             }
         }
     }
-}
-
-release {
-    // Disable the default checks since we're using GitHub Actions
-    failOnUnversionedFiles = false
-    failOnUpdateNeeded = false
-    failOnCommitNeeded = false
-    git {
-        requireBranch.set("") // Since we use GHA to release this should be okay
-        pushToRemote.set("origin")
-        signTag.set(false)
-        tagTemplate.set("v\${version}")
-    }
-}
-
-tasks.afterReleaseBuild {
-    dependsOn(tasks.publish)
 }
