15dd9642d2d9f587246c1d16b1e3186b842ae2df
Stephen Balogh <stephen@s2.dev>
2025-01-29 12:52:09 -0800

    clean imports

--- s2/build.gradle.kts
+++ s2/build.gradle.kts
@@ -10,37 +10,25 @@ repositories {
     mavenCentral()
 }
 
-val grpcVersion = "1.64.0"
-val protobufVersion = "3.25.0"
-val tomcatAnnotationsVersion = "11.0.2"
-val javaxAnnotationVersion = "1.3.2"
-val mockitoVersion = "5.8.0"
-val assertJVersion = "3.24.2"
-
 dependencies {
+    compileOnly(libs.tomcat.annotations)
+    implementation(libs.grpc.netty.shaded)
+    implementation(libs.grpc.protobuf)
+    implementation(libs.grpc.stub)
+    implementation(libs.javax.annotation.api)
+    implementation(libs.slf4j.api)
     implementation(project(":s2-internal"))
-
-    implementation("io.grpc:grpc-protobuf:$grpcVersion")
-    implementation("io.grpc:grpc-stub:$grpcVersion")
-    implementation("io.grpc:grpc-netty-shaded:$grpcVersion")
-    implementation("javax.annotation:javax.annotation-api:$javaxAnnotationVersion")
-    implementation("com.google.protobuf:protobuf-java:$protobufVersion")
-    implementation("org.slf4j:slf4j-api:1.7.32")
-
-    compileOnly("org.apache.tomcat:tomcat-annotations-api:$tomcatAnnotationsVersion")
-
-    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation(libs.assertj.core)
+    testImplementation(libs.grpc.inprocess)
+    testImplementation(libs.grpc.testing)
     testImplementation(libs.junit.jupiter)
-    testImplementation("io.grpc:grpc-testing:$grpcVersion")
-    testImplementation("io.grpc:grpc-inprocess:$grpcVersion")
-    testImplementation("org.mockito:mockito-core:$mockitoVersion")
-    testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion") {
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.junit.jupiter) {
         exclude(group = "org.junit.jupiter")
     }
-    testImplementation("uk.org.webcompere:system-stubs-jupiter:2.1.7")
-    testImplementation("org.assertj:assertj-core:$assertJVersion")
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(libs.system.stubs.jupiter)
+    testImplementation(platform(libs.junit.bom))
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 
