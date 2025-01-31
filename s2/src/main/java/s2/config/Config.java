23dc436c1e477cef4b06326b1fb587ae7a047d44
Stephen Balogh <stephen@s2.dev>
2025-01-30 22:24:52 -0800

    alpha

--- s2/src/main/java/s2/config/Config.java
+++ s2/src/main/java/s2/config/Config.java
@@ -6,31 +6,31 @@ import java.util.Optional;
 
 public class Config {
   public final String token;
-  public final Endpoints endpoints;
-  public final String userAgent;
-  public final Integer maxRetries;
-  public final Duration retryDelay;
-  public final Duration requestTimeout;
   public final AppendRetryPolicy appendRetryPolicy;
+  public final Endpoints endpoints;
   public final Integer maxAppendInflightBytes;
+  public final Integer maxRetries;
+  public final Duration requestTimeout;
+  public final Duration retryDelay;
+  public final String userAgent;
 
   private Config(
       String token,
-      Endpoints endpoints,
-      String userAgent,
-      Integer maxRetries,
-      Duration retryDelay,
-      Duration requestTimeout,
       AppendRetryPolicy appendRetryPolicy,
-      Integer maxAppendInflightBytes) {
+      Endpoints endpoints,
+      Integer maxAppendInflightBytes,
+      Integer maxRetries,
+      Duration requestTimeout,
+      Duration retryDelay,
+      String userAgent) {
     this.token = token;
-    this.endpoints = endpoints;
-    this.userAgent = userAgent;
-    this.maxRetries = maxRetries;
-    this.retryDelay = retryDelay;
-    this.requestTimeout = requestTimeout;
     this.appendRetryPolicy = appendRetryPolicy;
+    this.endpoints = endpoints;
     this.maxAppendInflightBytes = maxAppendInflightBytes;
+    this.maxRetries = maxRetries;
+    this.requestTimeout = requestTimeout;
+    this.retryDelay = retryDelay;
+    this.userAgent = userAgent;
   }
 
   public static ConfigBuilder newBuilder(String token) {
@@ -39,35 +39,20 @@ public class Config {
 
   public static final class ConfigBuilder {
     private final String token;
-    private Optional<String> userAgent = Optional.empty();
-    private Optional<Endpoints> endpoints = Optional.empty();
-    private Optional<Duration> requestTimeout = Optional.empty();
-    private Optional<Integer> maxRetries = Optional.empty();
-    private Optional<Duration> retryDelay = Optional.empty();
     private Optional<AppendRetryPolicy> appendRetryPolicy = Optional.empty();
+    private Optional<Endpoints> endpoints = Optional.empty();
     private Optional<Integer> maxAppendInflightBytes = Optional.empty();
+    private Optional<Integer> maxRetries = Optional.empty();
+    private Optional<Duration> requestTimeout = Optional.empty();
+    private Optional<Duration> retryDelay = Optional.empty();
+    private Optional<String> userAgent = Optional.empty();
 
     ConfigBuilder(String token) {
       this.token = token;
     }
 
-    public ConfigBuilder withUserAgent(String userAgent) {
-      this.userAgent = Optional.of(userAgent);
-      return this;
-    }
-
-    public ConfigBuilder withRequestTimeout(long timeout, TemporalUnit unit) {
-      this.requestTimeout = Optional.of(Duration.of(timeout, unit));
-      return this;
-    }
-
-    public ConfigBuilder withMaxRetries(int retries) {
-      this.maxRetries = Optional.of(retries);
-      return this;
-    }
-
-    public ConfigBuilder withRetryDelay(Duration delay) {
-      this.retryDelay = Optional.of(delay);
+    public ConfigBuilder withAppendRetryPolicy(AppendRetryPolicy appendRetryPolicy) {
+      this.appendRetryPolicy = Optional.of(appendRetryPolicy);
       return this;
     }
 
@@ -76,13 +61,28 @@ public class Config {
       return this;
     }
 
-    public ConfigBuilder withAppendRetryPolicy(AppendRetryPolicy appendRetryPolicy) {
-      this.appendRetryPolicy = Optional.of(appendRetryPolicy);
+    public ConfigBuilder withMaxAppendInflightBytes(int maxAppendInflightBytes) {
+      this.maxAppendInflightBytes = Optional.of(maxAppendInflightBytes);
       return this;
     }
 
-    public ConfigBuilder withMaxAppendInflightBytes(int maxAppendInflightBytes) {
-      this.maxAppendInflightBytes = Optional.of(maxAppendInflightBytes);
+    public ConfigBuilder withMaxRetries(int retries) {
+      this.maxRetries = Optional.of(retries);
+      return this;
+    }
+
+    public ConfigBuilder withRequestTimeout(long timeout, TemporalUnit unit) {
+      this.requestTimeout = Optional.of(Duration.of(timeout, unit));
+      return this;
+    }
+
+    public ConfigBuilder withRetryDelay(Duration delay) {
+      this.retryDelay = Optional.of(delay);
+      return this;
+    }
+
+    public ConfigBuilder withUserAgent(String userAgent) {
+      this.userAgent = Optional.of(userAgent);
       return this;
     }
 
@@ -90,13 +90,13 @@ public class Config {
       validate();
       return new Config(
           this.token,
-          this.endpoints.orElse(Endpoints.forCloud(Cloud.AWS)),
-          this.userAgent.orElse("s2-sdk-java"),
-          this.maxRetries.orElse(3),
-          this.retryDelay.orElse(Duration.ofMillis(50)),
-          this.requestTimeout.orElse(Duration.ofSeconds(10)),
           this.appendRetryPolicy.orElse(AppendRetryPolicy.ALL),
-          this.maxAppendInflightBytes.orElse(Integer.MAX_VALUE));
+          this.endpoints.orElse(Endpoints.forCloud(Cloud.AWS)),
+          this.maxAppendInflightBytes.orElse(Integer.MAX_VALUE),
+          this.maxRetries.orElse(3),
+          this.requestTimeout.orElse(Duration.ofSeconds(10)),
+          this.retryDelay.orElse(Duration.ofMillis(50)),
+          this.userAgent.orElse("s2-sdk-java"));
     }
 
     private void validate() {
